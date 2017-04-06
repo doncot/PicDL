c8559e519a96e99ef380352753a2a35c76b35172
doncot <doncot@gmail.com>
2017-04-06 23:22:14 +0900

    ボタンUIを作成

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-console.log("test");
+var placerNodes = document.getElementsByClassName("action");
+if(placerNodes.length > 0)
+{
+    // 名前 作品名取得
 
+    var placer = placerNodes[0];
+    var button = document.createElement("button");
+    button.innerText = "画像をダウンロード"
+    button.classList.toggle('_button');
+    button.type = "button"
+    button.style.float = "right";
+    button.style.marginTop = "10px";
+
+    button.onclick = function(){
+        alert("");
+    }
+    
+    placer.appendChild(button);
+}
+ 
